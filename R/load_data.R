39e7ff6a2f06b7f9fa9b5c6cce399d472fe231ea
leandroroser <learoser@gmail.com>
2022-12-13 05:29:22 -0300

    refactor

--- R/load_data.R
+++ R/load_data.R
@@ -16,7 +16,7 @@ read_data <- function(path, version = "latest") {
     clinvar <- sparklyr::spark_read_table(sc, "shinyapp_input")
     #clinvar <- sparklyr::spark_read_table(sc, "original_table")
     print("Transforming data...")
-    clinvar <- dplyr::select(clinvar, -c(hash, latest))
+    #clinvar <- dplyr::select(clinvar, -c(hash, latest))
     clinvar <- sparklyr::collect(clinvar)
     head(clinvar) # remove for final version
     clinvar <- as.data.frame(clinvar)

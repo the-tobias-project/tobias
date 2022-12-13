3690ea3861715ac16e8ec59d83123653e85766d1
leandroroser <learoser@gmail.com>
2022-12-13 05:04:23 -0300

    cleanup

--- tobias/R/load_data.R
+++ tobias/R/load_data.R
@@ -13,10 +13,10 @@ read_data <- function(path, version = "latest") {
     print("Connecting to database...")
     sparklyr::tbl_change_db(sc, "tobias")
     print("Reading data...")
-    #clinvar <- sparklyr::spark_read_table(sc, "shinyapp_input")
-    clinvar <- sparklyr::spark_read_table(sc, "original_table")
+    clinvar <- sparklyr::spark_read_table(sc, "shinyapp_input")
+    #clinvar <- sparklyr::spark_read_table(sc, "original_table")
     print("Transforming data...")
-    #clinvar <- dplyr::select(clinvar, -c(hash, latest))
+    clinvar <- dplyr::select(clinvar, -c(hash, latest))
     clinvar <- sparklyr::collect(clinvar)
     head(clinvar) # remove for final version
     clinvar <- as.data.frame(clinvar)

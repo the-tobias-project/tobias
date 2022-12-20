a010074338d021604bd286fb5ed212cc21813384
leandroroser <learoser@gmail.com>
2022-12-20 17:00:38 -0300

    update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,10 @@
-# TOFIX
+.onAttach <- function(libname, pkgname, spark_home) {
+  con <- DBI::dbConnect(odbc::odbc(), "Databricks")
+  assign("con", con, envir = .GlobalEnv)
+  packageStartupMessage("Databricks connected!")
+}
 
-#.onAttach <- function(libname, pkgname, spark_home) {
-#  con <- DBI::dbConnect(odbc::odbc(), "Databricks")
-#  assign("con", con, envir = .GlobalEnv)
-#  packageStartupMessage("Databricks connected!")
-#}
-
-#.onDetach <- function(libpath) {
-#  DBI::dbDisconnect(con)
-#  packageStartupMessage("Databricks disconnected!")
-#}
+.onDetach <- function(libpath) {
+  DBI::dbDisconnect(con)
+  packageStartupMessage("Databricks disconnected!")
+}

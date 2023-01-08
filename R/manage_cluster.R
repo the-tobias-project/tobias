9a476968e3d413bb15ccd7274584751bc3bc58ea
leandroroser <learoser@gmail.com>
2023-01-08 07:24:52 -0300

    add JDBC support

--- R/manage_cluster.R
+++ R/manage_cluster.R
@@ -2,8 +2,15 @@
 #' Start cluster
 #' @export
 
-connect_cluster <- function() {
+connect_cluster <- function(type=c("odbc", "jdbc")) {
+  if(type == "odbc") {
   con <- DBI::dbConnect(odbc::odbc(), "Databricks")
+  con
+  } else {
+    driver <- RJDBC::JDBC(driverClass = "com.databricks.client.jdbc.Driver",
+                   classPath = Sys.getenv("DATABRICKS_CLASSPATH")
+    con <- DBI::dbConnect(driver,Sys.getenv("JDBC_PATH"))
+  }
   con
 }
 

dd8c10dad5fd341d022b609f08b625bac2365b13
leandroroser <learoser@gmail.com>
2023-01-16 10:19:11 -0300

    Revert "add JDBC support"
    This reverts commit 9a476968e3d413bb15ccd7274584751bc3bc58ea.

--- R/manage_cluster.R
+++ R/manage_cluster.R
@@ -2,20 +2,8 @@
 #' Start cluster
 #' @export
 
-connect_cluster <- function(type=c("odbc", "jdbc", "sparklyr")) {
-  if(type == "odbc") {
+connect_cluster <- function() {
   con <- DBI::dbConnect(odbc::odbc(), "Databricks")
-  con
-  } else if(type=="jdbc") {
-    driver <- RJDBC::JDBC(driverClass = "com.databricks.client.jdbc.Driver",
-                   classPath = Sys.getenv("DATABRICKS_CLASSPATH"))
-    con <- DBI::dbConnect(driver, Sys.getenv("JDBC_PATH"))
-  } else if(type == "sparklyr"){
-    config <- spark_config()
-    config$`sparklyr.shell.driver-class-path` <- Sys.getenv("JDBC_PATH")
-    sc <- spark_connect(master = "local", config = config)
-  }
-  con$"type" <- type
   con
 }
 
@@ -24,9 +12,5 @@ connect_cluster <- function(type=c("odbc", "jdbc", "sparklyr")) {
 #' @export
 
 disconnect_cluster <- function(con) {
-  if(con$type %in% ("odbc", "jdbc")) {
-    DBI::dbDisconnect(con)
-  } else {
-    spark_disconnect(con)
-  }
+  DBI::dbDisconnect(con)
 }
